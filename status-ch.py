680a5a0094209cb6cc006ea366f361aac7124ee1
lokki <44494883+luckylokki@users.noreply.github.com>
2023-10-06 15:36:36 +0200

    main file final

--- status-ch.py
+++ status-ch.py
@@ -59,14 +59,18 @@ def check_services():
 def date_now_log():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+
 def cpu_perc():
     return psutil.cpu_percent(1)
+
 
 def vmem_perc():
     return psutil.virtual_memory().percent
 
+
 while True:
     service_list.clear()
+
     for check in check_services():
 
         if check[1] == False:
@@ -74,17 +78,13 @@ while True:
             data = str([date_now, name, check[0]])
             print(f'[!] {date_now_log()} Service {name} stopped')
             log_write(log_name, str(f'[!] {date_now_log()} Send {data}\n'))
-            slack_notification(f'{name}', f'{date_now_log()} Service {check[0]} stopped','#e01e5a')
-        # else:
-        #     date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        #     data = str([date_now, name, check[0]])
-        #     print(f'[+] {date_now_log()} Send {name} all is ok')
-        #     #slack_notification(f'{name}', f'{date_now_log()} Работает {data}','#f2c744')
+            slack_notification(f'{name}', f'{date_now_log()} Service {check[0]} stopped', '#e01e5a')
+
     cpu_p = cpu_perc()
     vmem_p = vmem_perc()
-    if int(cpu_p) > 60:
-        slack_notification(f'{name}', f'{date_now_log()} CPU is over 50% load! {cpu_p}','#e01e5a')
+    if int(cpu_p) > 80:
+        slack_notification(f'{name}', f'{date_now_log()} CPU is over 80% load! {cpu_p}', '#e01e5a')
 
-    if int(vmem_p) > 50:
+    if int(vmem_p) > 70:
         slack_notification(f'{name}', f'{date_now_log()} RAM is over 70% load! {vmem_p}', '#e01e5a')
-    time.sleep(3)
+    time.sleep(15)
