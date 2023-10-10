e6da4670dbace2b50eacb4055d889c7defb946d5
lokki <44494883+luckylokki@users.noreply.github.com>
2023-10-10 13:33:09 +0200

    install change

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ subprocess.call("sudo cp -r status-ch.service /etc/systemd/system/", shell=True)
 subprocess.call("sudo cp -r * /etc/status-ch", shell=True)
 subprocess.call("sudo rm -f /etc/status-ch/install.py", shell=True)
 subprocess.call("sudo python3 -m venv venv", shell=True, cwd='/etc/status-ch/')
-cmd = 'sudo source /etc/status-ch/venv/bin/activate; pip install -r requirements.txt; deactivate'
+cmd = 'source /etc/status-ch/venv/bin/activate; pip install -r requirements.txt; deactivate'
 subprocess.call(cmd, shell=True, executable='/bin/bash')
 subprocess.call("sudo systemctl enable status-ch", shell=True)
 subprocess.call("sudo systemctl daemon-reload", shell=True)
