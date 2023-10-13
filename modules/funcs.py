93366c76c3026f4dde3531c0d20b19743d3cfb43
lokki <44494883+luckylokki@users.noreply.github.com>
2023-10-13 14:47:49 +0200

    add 'On/Off'to all services. move functions to modules/func.py

--- modules/funcs.py
+++ modules/funcs.py
@@ -1,6 +1,10 @@
 import psutil
 from datetime import datetime
 import configparser
+import traceback
+import urllib3
+from modules import MyService
+import json
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -9,13 +13,53 @@ webhook_url = config['slack']['url'] + config['slack']['token']
 log_dir = config['agent-config']['log_pwd']
 log_name = log_dir + 'status-ch/' + datetime.today().strftime('%d-%m-%Y') + '.log'
 pm2_status = config['PM2']['pm2_service']
+service_s = config['services-check']['service_s']
 max_cpu = int(config['percents']['max_cpu'])
 max_vram = int(config['percents']['max_vram'])
 max_disk = int(config['percents']['max_disk'])
+cpu_s = config['percents']['cpu_s']
+vram_s = config['percents']['vram_s']
+disk_s = config['percents']['disk_s']
 stime = int(config['sleeptime']['stime'])
 service_list = []
+
+
+def slack_notification(who, message, color):
+    try:
+        slack_data = {
+            "attachments": [
+                {
+                    "color": color,
+                    "fields": [
+                        {
+                            "title": who,
+                            "value": message,
+                        }
+                    ]
+                }
+            ]
+        }
+        slack_message = {'text': message}
+
+        http = urllib3.PoolManager()
+        response = http.request('POST',
+                                webhook_url,
+                                body=json.dumps(slack_data),
+                                headers={'Content-Type': 'application/json'},
+                                retries=False)
+    except:
+        traceback.print_exc()
+
+    return True
+
+
 def date_now_log():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def log_write(file, data):
+    with open(file, 'a') as log:
+        log.write(data)
 
 
 def cpu_perc():
@@ -28,6 +72,31 @@ def vmem_perc():
 
 def disk_perc():
     return psutil.disk_usage('/').percent
+
+
+def check_services():
+    for key in config['services']:
+        service_list.append(MyService(config['services'][key]).check_service_status())
+    return service_list
+
+
+def check_cpu_ram():
+    cpu_p = cpu_perc()
+    vmem_p = vmem_perc()
+    disk_p = disk_perc()
+    if cpu_s == "On":
+        if int(cpu_p) > max_cpu:
+            slack_notification(f'{name}', f'{date_now_log()} {cpu_p}% of CPU used is over 80% load!', '#e01e5a')
+            log_write(log_name, str(f'[!] {date_now_log()} Server {name}: {cpu_p}% of CPU used is over 80% load!\n'))
+    if vram_s == "On":
+        if int(vmem_p) > max_vram:
+            slack_notification(f'{name}', f'{date_now_log()} {vmem_p}% of RAM used is over 80% load!', '#e01e5a')
+            log_write(log_name, str(f'[!] {date_now_log()} Server {name}: {vmem_p}% of RAM used is over 80% load!\n'))
+    if disk_s == "On":
+        if int(disk_p) > max_disk:
+            slack_notification(f'{name}', f'{date_now_log()} {disk_p}% of DISK space used is over 80% load!', '#e01e5a')
+            log_write(log_name,
+                      str(f'[!] {date_now_log()} Server {name}: {disk_p}% of DISK space used is over 80% load!\n'))
 
 
 if __name__ == "__main__":
--- status-ch.py
+++ status-ch.py
@@ -1,71 +1,9 @@
-from modules import MyService
 from modules.funcs import *
 import time
 import subprocess
-import urllib3
 import json
-import traceback
 from datetime import datetime
 import os
-
-
-def log_write(file, data):
-    with open(file, 'a') as log:
-        log.write(data)
-
-
-def slack_notification(who, message, color):
-    try:
-        slack_data = {
-            "attachments": [
-                {
-                    "color": color,
-                    "fields": [
-                        {
-                            "title": who,
-                            "value": message,
-                        }
-                    ]
-                }
-            ]
-        }
-        slack_message = {'text': message}
-
-        http = urllib3.PoolManager()
-        response = http.request('POST',
-                                webhook_url,
-                                body=json.dumps(slack_data),
-                                headers={'Content-Type': 'application/json'},
-                                retries=False)
-    except:
-        traceback.print_exc()
-
-    return True
-
-
-def check_services():
-    for key in config['services']:
-        service_list.append(MyService(config['services'][key]).check_service_status())
-    return service_list
-
-
-def check_cpu_ram():
-    cpu_p = cpu_perc()
-    vmem_p = vmem_perc()
-    disk_p = disk_perc()
-    if int(cpu_p) > max_cpu:
-        slack_notification(f'{name}', f'{date_now_log()} {cpu_p}% of CPU used is over 80% load!', '#e01e5a')
-        log_write(log_name, str(f'[!] {date_now_log()} Server {name}: {cpu_p}% of CPU used is over 80% load!\n'))
-
-    if int(vmem_p) > max_vram:
-        slack_notification(f'{name}', f'{date_now_log()} {vmem_p}% of RAM used is over 80% load!', '#e01e5a')
-        log_write(log_name, str(f'[!] {date_now_log()} Server {name}: {vmem_p}% of RAM used is over 80% load!\n'))
-
-    if int(disk_p) > max_disk:
-        slack_notification(f'{name}', f'{date_now_log()} {disk_p}% of DISK space used is over 80% load!', '#e01e5a')
-        log_write(log_name,
-                  str(f'[!] {date_now_log()} Server {name}: {disk_p}% of DISK space used is over 80% load!\n'))
-
 
 # main loop
 while True:
@@ -73,14 +11,15 @@ while True:
     # CPU RAM DISK check
     check_cpu_ram()
     # Service status check
-    for check in check_services():
-        if check[1] == False:
-            date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            data = str([date_now, name, check[0]])
-            print(f'[!] {date_now_log()} Service {check[0]} stopped or not exist, check it!')
-            log_write(log_name, str(f'[!] {date_now_log()} Service "{check[0]}" stopped or not exist, check it!\n'))
-            slack_notification(f'{name}', f'{date_now_log()} Service "{check[0]}" stopped or not exist, check it!',
-                               '#e01e5a')
+    if service_s == "On":
+        for check in check_services():
+            if check[1] == False:
+                date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                data = str([date_now, name, check[0]])
+                print(f'[!] {date_now_log()} Service {check[0]} stopped or not exist, check it!')
+                log_write(log_name, str(f'[!] {date_now_log()} Service "{check[0]}" stopped or not exist, check it!\n'))
+                slack_notification(f'{name}', f'{date_now_log()} Service "{check[0]}" stopped or not exist, check it!',
+                                   '#e01e5a')
     # PM2 status check
     if pm2_status == 'On':
         if os.path.isfile('/etc/status-ch/l1'):
